4d4234495bd8a8b11a52ff00f3cb7f36c2c8efa9
Daniil Dumshenko <dumshenko2003@gmail.com>
2024-10-08 09:08:11 +0200

    completed homework

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,11 @@ package org.example;
 import java.util.*;
 
 public class Main {
+    public static void main(String[] args) {
+        Random randomNumber = new Random();
 
+        int nextNumber = randomNumber.nextInt(0, 11);
+
+        System.out.println("Your lucky number today: " + nextNumber);
+    }
 }
